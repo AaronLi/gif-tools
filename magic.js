1d4829fa514c17f891876b14a83012f9b2268901
Aaron Li <dumfing@gmail.com>
2024-10-16 00:44:04 -0700

    Increase end frame time

--- magic.js
+++ magic.js
@@ -153,7 +153,7 @@ function doRender() {
     for(frame = 0; frame<=totalFrames; frame++){
         gif.addFrame(interpolateImages(startImageData.data, endImageData.data, frame/totalFrames, canvas.width), {copy: true, delay: 1000/generationSettings.framerate});
     }
-    gif.addFrame(endImageData, {copy: true, delay: 1500});
+    gif.addFrame(endImageData, {copy: true, delay: 2000});
 
     gif.on('finished', function(blob) {
         const gifUrl = URL.createObjectURL(blob);
